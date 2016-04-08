1bad927adbf8afaf138682555db99379423e1f3e
melliott03 <melliott03@gmail.com>
2016-04-07 20:23:57 -0500

    added text to phoneCall.xml

--- server/routes/phoneCall.js
+++ server/routes/phoneCall.js
@@ -13,18 +13,32 @@ var restler = require('restler');
 var sugar = require('sugar');
 
 router.get("/:id", function (req, res) {
+  console.log('req.params.id inside /phoneCall/:id');
+  console.log('req.params.id inside /phoneCall/:id', req.params.id);
+
   var builder = require('xmlbuilder');
-  var xml = builder.create('root')
-  .ele('xmlbuilder')
-  .ele('repo', {'type': 'git'}, 'git://github.com/oozcitak/xmlbuilder-js.git')
+  var xml = builder.create('Response')
+  .ele('Say')
+  .ele('Say', {'voice': 'Alice'}, 'This is a message from the Michael application. Goodbye.')
   .end({ pretty: true});
   console.log(xml);
-
-console.log('req.params.id inside /phoneCall/:id');
-
-console.log('req.params.id inside /phoneCall/:id', req.params.id);
-res.send({"params" : ''+req.params.id});
+res.send({xml});
 });
+// 
+// <?xml version="1.0"?>
+// <root>
+//   <xmlbuilder>
+//     encoding
+//     <repo type="git">git://github.com/oozcitak/xmlbuilder-js.git</repo>
+//   </xmlbuilder>
+// </root>
+//
+//
+// <?xml version="1.0" encoding="UTF-8"?>
+//  <Response>
+//  <Say voice="alice">To get connected to your interpreting sesson, enter your 4 digit key</Say>
+//  <Play>http://demo.twilio.com/docs/classic.mp3</Play>
+//  </Response>
 
 
 
