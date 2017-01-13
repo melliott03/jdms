b81cae817e84637d66c723b10911493a3352567b
melliott03 <melliott03@gmail.com>
2017-01-12 19:21:06 -0600

    adding bookingid and language to DOM through socketio

--- server/public/assets/scripts/controllers.js
+++ server/public/assets/scripts/controllers.js
@@ -699,7 +699,6 @@ myApp.config(['ChartJsProvider', function (ChartJsProvider) {
           Socket.addListener('newRemoveBookingItem', function (msg) {
             console.log("in AddController new Socket Alert newRemoveBookingItem, msg::", msg);
             console.log("in AddController new Socket Alert newRemoveBookingItem, $scope.bookings::", $scope.bookings);
-            var _id = msg._id;
             $scope.bookings = $scope.bookings.filter(function( obj ) {
               return obj._id !== _id;
             });
