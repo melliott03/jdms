0ae81abf4c2fea5d40a8ca1f5540c0bf9bf60587
melliott03 <melliott03@gmail.com>
2017-01-14 11:29:39 -0600

    adding blinking green light to worker availability

--- server/public/assets/vendors/angular-points-path/src/angular-points-path.js
+++ server/public/assets/vendors/angular-points-path/src/angular-points-path.js
@@ -496,6 +496,8 @@ angular.module('angular-points-path')
         //// if delta <=0 or >=1 then reverse
         if (alpha <= 0 || alpha >= 1) {
           ctrl.context.clip();
+          ctrl.context.clearRect(data.x, data.y, data.value, 0);
+          // ctrl.context.clearRect(0, 0, ctrl.canvas.width, ctrl.canvas.height);
           delta = -delta;
         }else {
           // ctrl.context.clearRect(0, 0, ctrl.canvas.width, ctrl.canvas.height);
